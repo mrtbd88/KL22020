717b49ec522400f0492c2c9a355e17c1188b24cb
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 17:37:50 -0400

    done class path

--- src/kl22020/Kl2Test.java
+++ src/kl22020/Kl2Test.java
@@ -2,10 +2,12 @@ package kl22020;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Kl2Test {
 
-	public static void main(String[] args) {
+	@Test
+	public void openBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\KL22020\\drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.webull.com/");
