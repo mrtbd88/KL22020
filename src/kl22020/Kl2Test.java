efc83d90f804363186b2d19d0efd6d1202e98a79
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 17:29:23 -0400

    kl2 test

--- src/kl22020/Kl2Test.java
+++ src/kl22020/Kl2Test.java
@@ -6,8 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Kl2Test {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\KL22020\\drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.webull.com/");
 	}
 
 }
