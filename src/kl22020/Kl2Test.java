b20f49873d728c049b2a3bf148fe9d0804c6377c
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 16:59:42 -0400

    kl stage

--- src/kl22020/Kl2Test.java
+++ src/kl22020/Kl2Test.java
@@ -1,10 +1,13 @@
 package kl22020;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Kl2Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 	}
 
 }
